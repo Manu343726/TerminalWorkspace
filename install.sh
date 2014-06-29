bdc159c3c3e70a490f8eba63b1c2d4eec8c73936
Manu343726 <Manu343726@gmail.com>
2014-06-29 15:37:12 +0200

    sync.sh deprecated. sync_home.sh for repo -> home sync and sync_repo.sh for home -> repo sync

--- install.sh
+++ install.sh
@@ -6,35 +6,6 @@ sudo yaourt -S --noconfirm --needed gvim tmux oh-my-zsh-git fbterm acpi
 echo "Setting up zsh..."
 chsh -s /bin/zsh
 
-echo "Copying config files..."
-
-#Copy configuration files to their locations:
-cp ./scripts/.vimrc ~/.vimrc -f -v
-cp ./scripts/.zshrc ~/.zshrc -f -v
-cp ./scripts/.tmux.conf ~/.tmux.conf -f -v
-cp ./scripts/.fbtermrc ~/.fbtermrc -f -v
-sudo cp ./scripts/profile /etc/profile -f -v
-
-echo "Installing vim plugins..."
-vim -c "BundleInstall" -c qall
-
-
-echo "Copying presets..."
-
-#Copy tmuxline full preset:
-cp ./scripts/tmuxline/presets/full.vim ~/.vim/bundle/tmuxline.vim/autoload/tmuxline/presets/full.vim -f -v
-
-#Copy tmuxline status scripts to /usr/local/bin
-sudo cp ./scripts/status/* /usr/local/bin/ -f -v
-
-
-echo "Generating snapshots..."
-
-#Generate configuration scripts for tmuxline and promptline:
-vim -c "TmuxlineSnapshot! ~/.tmuxline_cfg" -c qall
-vim -c "PromptlineSnapshot! ~/.shell_prompt.sh airline" -c qall
-source ~/.shell_prompt.sh
-
-
-echo "Done"
+echo "Syncing home config..."
+./sync_home.sh
 
