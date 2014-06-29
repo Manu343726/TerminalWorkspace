b46b9372022c9f7ec47e51de0c98541eb37ed26f
Manu343726 <Manu343726@gmail.com>
2014-06-29 17:47:15 +0200

    Mmmm...

--- sync_home.sh
+++ sync_home.sh
@@ -3,11 +3,11 @@
 echo "Copying config files..."
 
 #Copy configuration files to their locations:
-cp ./scripts/.vimrc ~/.vimrc -f -v
-cp ./scripts/.zshrc ~/.zshrc -f -v
-cp ./scripts/.tmux.conf ~/.tmux.conf -f -v
-cp ./scripts/.fbtermrc ~/.fbtermrc -f -v
-sudo cp ./scripts/profile /etc/profile -f -v
+cp ~/TerminalWorkspace/scripts/.vimrc ~/.vimrc -f -v
+cp ~/TerminalWorkspace/scripts/.zshrc ~/.zshrc -f -v
+cp ~/TerminalWorkspace/scripts/.tmux.conf ~/.tmux.conf -f -v
+cp ~/TerminalWorkspace/scripts/.fbtermrc ~/.fbtermrc -f -v
+sudo cp ~/TerminalWorkspace/scripts/profile /etc/profile -f -v
 
 echo "Installing vim plugins..."
 vim "BundleInstall" -c qall
@@ -16,10 +16,10 @@ vim "BundleInstall" -c qall
 echo "Copying presets..."
 
 #Copy tmuxline full preset:
-cp ./scripts/tmuxline/presets/full.vim ~/.vim/bundle/tmuxline.vim/autoload/tmuxline/presets/full.vim -f -v
+cp ~/TerminalWorkspace/scripts/tmuxline/presets/full.vim ~/.vim/bundle/tmuxline.vim/autoload/tmuxline/presets/full.vim -f -v
 
 #Copy tmuxline status scripts to /usr/local/bin
-sudo cp ./scripts/status/* /usr/local/bin/ -f -v
+sudo cp ~/TerminalWorkspace/scripts/status/* /usr/local/bin/ -f -v
 
 
 #Apply current configuration
--- sync_repo.sh
+++ sync_repo.sh
@@ -5,20 +5,20 @@ echo "Syncing repository config..."
 
 echo "Copying configuration files..."
 #Copy configuration files to their locations:
-cp ~/.vimrc ./scripts/.vimrc -f -v
-cp ~/.zshrc ./scripts/.zshrc -f -v
-cp ~/.tmux.conf ./scripts/.tmux.conf -f -v
-cp ~/.fbtermrc ./scripts/.fbtermrc -f -v
-cp /etc/profile ./scripts/profile -f -v
+cp ~/.vimrc ~/TerminalWorkspace/scripts/.vimrc -f -v
+cp ~/.zshrc ~/TerminalWorkspace/scripts/.zshrc -f -v
+cp ~/.tmux.conf ~/TerminalWorkspace/scripts/.tmux.conf -f -v
+cp ~/.fbtermrc ~/TerminalWorkspace/scripts/.fbtermrc -f -v
+cp /etc/profile ~/TerminalWorkspace/scripts/profile -f -v
 
 echo "Copying presets..."
 #Copy tmuxline full preset:
-cp ~/.vim/bundle/tmuxline.vim/autoload/tmuxline/presets/full.vim ./scripts/tmuxline/presets/full.vim -f -v
+cp ~/.vim/bundle/tmuxline.vim/autoload/tmuxline/presets/full.vim ~/TerminalWorkspace/scripts/tmuxline/presets/full.vim -f -v
 
 #Copy tmuxline status scripts to /usr/local/bin
-cp /usr/local/bin/* ./scripts/status/ -f -v
+cp /usr/local/bin/* ~/TerminalWorkspace/scripts/status/ -f -v
 
 #Apply current configuration:
-. ./apply_config.sh
+. ~/TerminalWorkspace/apply_config.sh
 
 echo "Done"
