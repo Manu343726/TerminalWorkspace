a23a0acad88154a499f288f9adb2b4afca09eb67
Manu343726 <Manu343726@gmail.com>
2014-06-29 20:32:25 +0200

    git fixes on pull/push scripts

--- pull.sh
+++ pull.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+TW_PATH = ~/TerminalWorkspace
+CURRENT_PATH = ./
+
+cd $TW_PATH
 git reset --hard HEAD
 git pull
-. ~/TerminalWorkspace/sync_home.sh
+. $TW_PATH/sync_home.sh
+cd $CURRENT_PATH
--- push.sh
+++ push.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-. ~/TerminalWorkspace/sync_repo.sh
-cd ~/TerminalWorkspace/
+TW_PATH = ~/TerminalWorkspace
+CURRENT_PATH = ./
+
+. $TW_PATH/sync_repo.sh
+
+cd $TWPATH
 git add -A
 git commit -m "Automatic config update (PUSH) from $(whoami)@$(uname --all)"
 git push
+
+cd $CURRENT_PATH
